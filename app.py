2bc1a07ca37b4192484d74b903fa801a8e8223dd
Ruslan Idelfonso Magana Vsevolodovna <ruslan.idelfonso.magana.vsevolodovna-cic@ibm.com>
2024-05-28 19:31:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,34 +3,43 @@ from dotenv import load_dotenv
 import streamlit as st
 import webchat
 import utils
+
 # URL of the hosted LLMs is hardcoded because at this time all LLMs share the same endpoint
 url = "https://us-south.ml.cloud.ibm.com"
-# These global variables will be updated in get_credentials() function
-watsonx_project_id = ""
-api_key = ""
+
 def main():
-    utils.get_credentials()
+    # Initialize session state for credentials
+    if 'watsonx_project_id' not in st.session_state:
+        st.session_state['watsonx_project_id'] = ""
+    if 'api_key' not in st.session_state:
+        st.session_state['api_key'] = ""
+
     st.set_page_config(layout="wide", page_title="RAG Web Demo", page_icon="")
     utils.load_css("styles.css")
+    
     # Streamlit app title with style
     st.markdown("""
         <div class="menu-bar">
             <h1>IBM watsonx.ai - webchat</h1>
         </div>
         <div style="margin-top: 20px;"><p>Insert the website you want to chat with and ask your question.</p></div>
-        
     """, unsafe_allow_html=True)
+    
     # Sidebar for settings
     st.sidebar.header("Settings")
-    st.sidebar.markdown("Insert your credentials of [IBM Cloud](https://cloud.ibm.com/login) for watsonx.ai \n The data is not saved  in th server. Your data  is secured.", unsafe_allow_html=True)
+    st.sidebar.markdown("Insert your credentials of [IBM Cloud](https://cloud.ibm.com/login) for watsonx.ai \n The data is not saved in the server. Your data is secured.", unsafe_allow_html=True)
     st.sidebar.markdown("<hr>", unsafe_allow_html=True)
-    api_key_input = st.sidebar.text_input("API Key", api_key, type="password")
-    project_id_input = st.sidebar.text_input("Project ID", watsonx_project_id)
-    # Update credentials if provided by the user
+    
+    # Input fields for API Key and Project ID
+    api_key_input = st.sidebar.text_input("API Key", st.session_state['api_key'], type="password")
+    project_id_input = st.sidebar.text_input("Project ID", st.session_state['watsonx_project_id'])
+    
+    # Update session state with the provided credentials
     if api_key_input:
-        globals()["api_key"] = api_key_input
+        st.session_state['api_key'] = api_key_input
     if project_id_input:
-        globals()["watsonx_project_id"] = project_id_input
+        st.session_state['watsonx_project_id'] = project_id_input
+    
     # Main input area
     user_url = st.text_input('Provide a URL')
     # UI component to enter the question
@@ -38,14 +47,14 @@ def main():
     button_clicked = st.button("Answer the question")
     st.markdown("<hr>", unsafe_allow_html=True)
     st.subheader("Response")
-    collection_name="base"
-    client=utils.chromadb_client()
-    if globals()["api_key"] and globals()["watsonx_project_id"]:
-        # Provide a unique name for this website (lower case). Use the same name for the same URL to avoid loading data multiple times.
-        #collection_name = utils.create_collection_name(user_url)
+    
+    collection_name = "base"
+    client = utils.chromadb_client()
+    
+    if st.session_state['api_key'] and st.session_state['watsonx_project_id']:
         if button_clicked and user_url:
             # Invoke the LLM when the button is clicked
-            response = webchat.answer_questions_from_web(api_key, watsonx_project_id, user_url, question, collection_name,client)
+            response = webchat.answer_questions_from_web(st.session_state['api_key'], st.session_state['watsonx_project_id'], user_url, question, collection_name, client)
             st.write(response)
     else:
         st.warning("Please provide API Key and Project ID in the sidebar.")
@@ -54,8 +63,8 @@ def main():
     st.sidebar.markdown("<hr>", unsafe_allow_html=True)
     st.sidebar.header("Memory")
     clean_button_clicked = st.sidebar.button("Clean Memory")
-    if clean_button_clicked :
-        if collection_name:  # Check if collection_name is defined and not empty
+    if clean_button_clicked:
+        if collection_name:
             utils.clear_collection(collection_name, client)
             st.sidebar.success("Memory cleared successfully!")
             print("Memory cleared successfully!")
