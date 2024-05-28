120d6fd423be7705a98bb5d8b49839a3705a66a0
Ruslan Idelfonso Magana Vsevolodovna <ruslan.idelfonso.magana.vsevolodovna-cic@ibm.com>
2024-05-28 11:44:55 +0200

    updates
    updates

--- app.py
+++ app.py
@@ -1,112 +1,17 @@
-# For reading credentials from the .env file
 import os
 from dotenv import load_dotenv
 import streamlit as st
 import webchat
-
+import utils
 # URL of the hosted LLMs is hardcoded because at this time all LLMs share the same endpoint
 url = "https://us-south.ml.cloud.ibm.com"
-
 # These global variables will be updated in get_credentials() function
 watsonx_project_id = ""
-# Replace with your IBM Cloud key
 api_key = ""
-
-def get_credentials():
-    load_dotenv()
-    # Update the global variables that will be used for authentication in another function
-    globals()["api_key"] = os.getenv("api_key", None)
-    globals()["watsonx_project_id"] = os.getenv("project_id", None)
-
-
-def set_theme():
-    st.markdown("""
-        <style>
-            .reportview-container, .main {
-                background: #ffffff;
-                color: #000000;
-            }
-            .sidebar .sidebar-content {
-                background: #f0f2f6;
-                color: #000000;
-            }
-            .stButton>button {
-                background-color: #0D62FE;
-                color: white;
-            }
-            .stTextInput>div>div>input {
-                color: #000000;
-                background-color: #ffffff;
-            }
-            tTextArea>div>textarea {
-                color: #000000;
-                background-color: #ffffff;
-            }
-            label, .stTextInput>label, .stTextArea>label {
-                color: #000000;
-            }
-            h1, h2, h3, h4, h5, h6 {
-                color: #000000;
-            }
-            .sidebar .sidebar-content h2, .sidebar .sidebar-content h3, .sidebar .sidebar-content h4, .sidebar .sidebar-content h5, .sidebar .sidebar-content h6, 
-            .sidebar .sidebar-content label, .sidebar .sidebar-content .stTextInput>label, .sidebar .sidebar-content .stTextArea>label {
-                color: #000000;
-            }
-            .navbar {
-                overflow: hidden;
-                background-color: #000000;
-                position: fixed;
-                top: 0;
-                width: 100%;
-                z-index: 1000;
-            }
-            .navbar h1 {
-                float: left;
-                display: block;
-                color: #ffffff;
-                text-align: center;
-                padding: 14px 1x;
-                text-decoration: none;
-                font-size: 17px;
-                margin: 0;  
-            }
-            .menu-bar {
-                background-color: #000000;
-                padding: 10px;
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-            }
-            .menu-bar h1 {
-                color: #ffffff;
-                margin: 0;
-                font-size: 18px; /* Reduced font size */
-                font-family: 'IBM Plex Sans', sans-serif; /* IBM font */
-            }
-        </style>
-    """, unsafe_allow_html=True)
-
-
-from urllib.parse import urlparse
-
-def create_collection_name(url):
-    parsed_url = urlparse(url)
-    domain_parts = parsed_url.netloc.split('.')
-    if len(domain_parts) >= 2:
-        return domain_parts[-2]  # Extracting the second-level domain
-    else:
-        return "base"
-
 def main():
-    # Get the API key and project id and update global variables
-    get_credentials()
-
-    # Use the full page instead of a narrow central column
+    utils.get_credentials()
     st.set_page_config(layout="wide", page_title="RAG Web Demo", page_icon="")
-
-    # Set the theme
-    set_theme()
-
+    utils.load_css("styles.css")
     # Streamlit app title with style
     st.markdown("""
         <div class="menu-bar">
@@ -115,37 +20,50 @@ def main():
         <div style="margin-top: 20px;"><p>Insert the website you want to chat with and ask your question.</p></div>
         
     """, unsafe_allow_html=True)
-
     # Sidebar for settings
     st.sidebar.header("Settings")
-    st.sidebar.markdown("Insert your credentials of [IBM Cloud](https://cloud.ibm.com/login) for watsonx.ai", unsafe_allow_html=True)
+    st.sidebar.markdown("Insert your credentials of [IBM Cloud](https://cloud.ibm.com/login) for watsonx.ai \n The data is not saved  in th server. Your data  is secured.", unsafe_allow_html=True)
     st.sidebar.markdown("<hr>", unsafe_allow_html=True)
     api_key_input = st.sidebar.text_input("API Key", api_key, type="password")
     project_id_input = st.sidebar.text_input("Project ID", watsonx_project_id)
-
     # Update credentials if provided by the user
     if api_key_input:
         globals()["api_key"] = api_key_input
     if project_id_input:
         globals()["watsonx_project_id"] = project_id_input
-
     # Main input area
-    #st.markdown("<hr>", unsafe_allow_html=True)
     user_url = st.text_input('Provide a URL')
-    # Provide a unique name for this website (lower case). Use the same name for the same URL to avoid loading data multiple times.
-    collection_name = create_collection_name(user_url)
     # UI component to enter the question
     question = st.text_area('Question', height=100)
     button_clicked = st.button("Answer the question")
-
     st.markdown("<hr>", unsafe_allow_html=True)
     st.subheader("Response")
+    collection_name="base"
+     ## Cleaning Vector Database
+    #utils.clear_collection(collection_name)
+    if globals()["api_key"] and globals()["watsonx_project_id"]:
+        # Provide a unique name for this website (lower case). Use the same name for the same URL to avoid loading data multiple times.
+        #collection_name = utils.create_collection_name(user_url)
+        if button_clicked and user_url:
+            # Invoke the LLM when the button is clicked
+            response = webchat.answer_questions_from_web(api_key, watsonx_project_id, user_url, question, collection_name)
+            st.write(response)
 
-    # Invoke the LLM when the button is clicked
-    if button_clicked:
-        response = webchat.answer_questions_from_web(api_key, watsonx_project_id, user_url, question, collection_name)
-        st.write(response)
-
+    else:
+        st.warning("Please provide API Key and Project ID in the sidebar.")
+  
+    # Cleaning Vector Database
+    st.sidebar.markdown("<hr>", unsafe_allow_html=True)
+    st.sidebar.header("Memory")
+    clean_button_clicked = st.sidebar.button("Clean Memory")
+    if clean_button_clicked :
+        if collection_name:  # Check if collection_name is defined and not empty
+            utils.clear_collection(collection_name)
+            st.sidebar.success("Memory cleared successfully!")
+            #st.sidebar.markdown(collection_name, unsafe_allow_html=True)
+            print("Memory cleared successfully!")
+        else:
+            st.sidebar.error("Collection name is not defined or empty.")
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,36 @@
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import os
+import chromadb
+import streamlit as st
+def get_credentials():
+    load_dotenv()
+    globals()["api_key"] = os.getenv("api_key", None)
+    globals()["watsonx_project_id"] = os.getenv("project_id", None)
+
+def load_css(file_name):
+    with open(file_name) as file:
+        st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
+
+def create_collection_name(url):
+    parsed_url = urlparse(url)
+    domain_parts = parsed_url.netloc.split('.')
+    if len(domain_parts) >= 2:
+        return domain_parts[-2]  # Extracting the second-level domain
+    else:
+        return "base"
+
+
+
+def clear_collection(collection_name):
+    client = chromadb.Client()
+    try:
+        collection = client.get_collection(collection_name)
+        if collection:
+            collection.delete()
+            print(f"Collection '{collection_name}' cleared successfully!")
+    except ValueError:
+        pass  # collection does not exist, do nothing
+
+
+
